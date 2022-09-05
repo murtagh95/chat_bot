055d43102b87f5b4050da0b3d479fded9d6a213b
Murtagh95 <nec.catalano@gmail.com>
2022-09-04 21:54:02 -0300

    feat(config/static): add static file confing

---
+++ app/config/static.py
@@ -0,0 +1,8 @@
+""" Static file setyp config """
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+
+def add_static(app: FastAPI):
+    app.mount("/public", StaticFiles(directory="public"), name="public")
