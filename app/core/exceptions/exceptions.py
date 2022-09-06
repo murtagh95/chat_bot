15262bab2cd5e88fc3da697caa73d9937d415da5
Murtagh95 <nec.catalano@gmail.com>
2022-09-05 22:12:55 -0300

    feat(exceptions): add base exception

--- app/core/exceptions/exceptions.py
+++ app/core/exceptions/exceptions.py
@@ -1,1 +1,53 @@
 """API Exceptions"""
+from pydantic import BaseModel
+
+
+class HTTPError(BaseModel):
+    """ HTTP error """
+    detail: str
+
+    class Config:
+        """" Config class """
+        schema_extra = {
+            "example": {"detail": "HTTPException raised."},
+        }
+
+
+class ValidationError(BaseModel):
+    loc: list
+    msg: str
+    type: str
+
+
+class HTTPValidationError(BaseModel):
+    """ Validation Error """
+    detail: list[ValidationError]
+
+    class Config:
+        """" Config class """
+        schema_extra = {
+            "example": {
+                "detail": [
+                    {
+                        "loc": [
+                            "string",
+                            0
+                        ],
+                        "msg": "string",
+                        "type": "string"
+                    }
+                ]
+            }
+        }
+
+
+RESPONSE_DICT_WITH_ERROR = {
+    404: {
+        "model": HTTPError,
+        "description": "This endpoint always raises an error",
+    },
+    422: {
+        "model": ValidationError,
+        "description": "Validation Error"
+    }
+}
