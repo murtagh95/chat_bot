7012ab06846cbb39442086608ea7da5156804c26
Murtagh95 <nec.catalano@gmail.com>
2022-09-10 22:48:49 -0300

    fix(way_schemas): unused pydantic schema deleted

--- app/core/models/tortoise/schemas/way_schemas.py
+++ app/core/models/tortoise/schemas/way_schemas.py
@@ -8,6 +8,4 @@ from app.core.models.tortoise.way import Way
 
 
 way_pydantic = pydantic_model_creator(Way, name="Way")
-way_pydantic_in = pydantic_model_creator(
-    Way, name="WayIn", exclude_readonly=True)
 way_pydantic_list = pydantic_queryset_creator(Way)
