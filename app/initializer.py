05c8f54f36ecf57ba974a57b90f8b6d8891fafc4
Murtagh95 <nec.catalano@gmail.com>
2022-09-10 21:57:51 -0300

    feat(initializer): add way router

--- app/initializer.py
+++ app/initializer.py
@@ -42,9 +42,11 @@ def init_routers(app: FastAPI):
     from app.core.routers import hello
     from app.core.routers import user_router
     from app.core.routers import message_router
+    from app.core.routers import way_router
 
     app.include_router(router=hello.router, prefix="/hello", tags=["hello"])
     app.include_router(router=user_router.router, prefix="/user", tags=["user"])
     app.include_router(router=message_router.router,
                        prefix="/message",
                        tags=["message"])
+    app.include_router(router=way_router.router, prefix="/way", tags=["way"])
